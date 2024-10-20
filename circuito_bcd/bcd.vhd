7eae8e963cd1f37de12c8cc9562d888f3985629a
Enio-Antonio <enioaclizieri@gmail.com>
2024-10-20 19:42:00 -0300

    Correção casos nulos

--- circuito_bcd/bcd.vhd
+++ circuito_bcd/bcd.vhd
@@ -116,44 +116,26 @@ begin
 	-- Saída s0
 	u1: PortaAnd port map(a => a, b => not(b), c => not(c), d => not(d), saida => si18);
 	u2: PortaAnd port map(a => not(a), b => not(b), c => c, d => not(d), saida => si19);
-	u28: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si20);
-	u35: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si21);
-	u42: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si22);
-	u49: PortaAnd port map(a => a, b => b, c => c, d => not(d), saida => si23);
-	u56: PortaAnd port map(a => a, b => not(b), c => c, d => not(d), saida => si24);
-	u63: PortaAnd port map(a => a, b => b, c => not(c), d => not(d), saida => si25);
 
-	u3: PortaOr2 port map(a => si18, b => si19, c => si20, d => si21, e => si22, f => si23, g => si24, h => si25, saida => s0);
+	u3: PortaOr2 port map(a => si18, b => si19, c => si20, d => si27, e => si34, f => si41, g => si48, h => si55, saida => s0);
 	
 	-- Saída s1
 	u4: PortaAnd port map(a => a, b => not(b), c => c, d => not(d), saida => si16);
 	u5: PortaAnd port map(a => not(a), b => b, c => c, d => not(d), saida => si17);
-	u29: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si27);
-	u36: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si28);
-	u43: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si29);
-	u50: PortaAnd port map(a => a, b => b, c => c, d => not(d), saida => si30);
-	u57: PortaAnd port map(a => a, b => not(b), c => c, d => not(d), saida => si31);
-	u64: PortaAnd port map(a => a, b => b, c => not(c), d => not(d), saida => si32);
 	
-	u6: PortaOr2 port map(a => si16, b => si17, c => si27, d => si28, e => si29, f => si30, g => si31, h => si32, saida => s1);
+	u6: PortaOr2 port map(a => si16, b => si17, c => si21, d => si28, e => si35, f => si42, g => si49, h => si56, saida => s1);
 	
 	-- Saída s2
 	u7: PortaAnd port map(a => not(a), b => b, c => not(c), d => not(d), saida => si63);
-	u30: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si33);
-	u37: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si34);
-	u44: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si35);
-	u51: PortaAnd port map(a => a, b => b, c => c, d => not(d), saida => si36);
-	u58: PortaAnd port map(a => a, b => not(b), c => c, d => not(d), saida => si37);
-	u65: PortaAnd port map(a => a, b => b, c => not(c), d => not(d), saida => si38);
 	
-	u70: PortaOr7 port map(a => si63, b => si33, c => si34, d => si35, e => si36, f => si37, g => si38, saida => s2);
+	u70: PortaOr7 port map(a => si63, b => si22, c => si29, d => si36, e => si43, f => si50, g => si57, saida => s2);
 	
 	-- Saída s3
 	u8: PortaAnd port map(a => a, b => not(b), c => not(c), d => not(d), saida => si13);
 	u9: PortaAnd port map(a => not(a), b => not(b), c => c, d => not(d), saida => si14);
 	u10: PortaAnd port map(a => a, b => b, c => c, d => not(d), saida => si15);
 	
-	u11: PortaOr3 port map(a => si13, b => si14, c => si15, d => si39, e => si40, f => si41, g => si42, h => si43, i => si44, saida => s3);
+	u11: PortaOr3 port map(a => si13, b => si14, c => si15, d => si23, e => si30, f => si37, g => si44, h => si51, i => si58, saida => s3);
 	
 	-- Saída s4
 	u12: PortaAnd port map(a => a, b => not(b), c => not(c), d => not(d), saida => si8);
@@ -163,7 +145,7 @@ begin
 	u16: PortaAnd port map(a => a, b => b, c => c, d => not(d), saida => si11);
 	u17: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si12);
 	
-	u18: PortaOr6 port map(a => si8, b => si9, c => si95, d => si10, e => si11, f => si12, g => si45, h => si46, i => si47, j => si48, k => si49, l => si50, saida => s4);
+	u18: PortaOr6 port map(a => si8, b => si9, c => si95, d => si10, e => si11, f => si12, g => si24, h => si31, i => si38, j => si45, k => si52, l => si59, saida => s4);
 	
 	-- Saída s5
 	u19: PortaAnd port map(a => a, b => not(b), c => not(c), d => not(d), saida => si4);
@@ -171,45 +153,64 @@ begin
 	u21: PortaAnd port map(a => a, b => b, c => not(c), d => not(d), saida => si6);
 	u22: PortaAnd port map(a => a, b => b, c => c, d => not(d), saida => si7);
 	
-	u23: portaOr4 port map(a => si4, b => si5, c => si6, d => si7, e => si51, f => si52, g => si53, h => si54, i => si55, j => si56, saida => s5);
+	u23: portaOr4 port map(a => si4, b => si5, c => si6, d => si7, e => si25, f => si32, g => si39, h => si46, i => si53, j => si60, saida => s5);
 	
 	-- Saída s6
 	u24: PortaAnd port map(a => not(a), b => not(b), c => not(c), d => not(d), saida => si1);
 	u25: PortaAnd port map(a => a, b => not(b), c => not(c), d => not(d), saida => si2);
 	u26: PortaAnd port map(a => a, b => b, c => c, d => not(d), saida => si3);
 	
-	u27: portaOr3 port map(a => si1, b => si2, c => si3, d => si57, e => si58, f => si59, g => si60, h => si61, i => si62, saida => s6);
+	u27: portaOr3 port map(a => si1, b => si2, c => si3, d => si26, e => si33, f => si40, g => si47, h => si54, i => si61, saida => s6);
 	
 	-- Os outros casos de saída para serem desconsiderados
-	u31: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si39);
-	u32: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si45);
-	u33: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si51);
-	u34: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si57);
+	u28: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si20);
+	u29: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si21);
+	u30: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si22);
+	u31: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si23);
+	u32: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si24);
+	u33: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si25);
+	u34: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si26);
 	
-	u38: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si40);
-	u39: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si46);
-	u40: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si52);
-	u41: PortaAnd port map(a => a, b => not(b), c => c, d => d, saida => si58);
+	u35: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si27);
+	u36: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si28);
+	u37: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si29);
+	u38: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si30);
+	u39: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si31);
+	u40: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si32);
+	u41: PortaAnd port map(a => a, b => b, c => not(c), d => d, saida => si33);
 	
-	u45: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si41);
-	u46: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si47);
-	u47: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si53);
-	u48: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si59);
+	u42: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si34);
+	u43: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si35);
+	u44: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si36);
+	u45: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si37);
+	u46: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si38);
+	u47: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si39);
+	u48: PortaAnd port map(a => a, b => b, c => c, d => d, saida => si40);
 	
-	u52: PortaAnd port map(a => a, b => b, c => c, d => not(d), saida => si42);
-	u53: PortaAnd port map(a => a, b => b, c => c, d => not(d), saida => si48);
-	u54: PortaAnd port map(a => a, b => b, c => c, d => not(d), saida => si54);
-	u55: PortaAnd port map(a => a, b => b, c => c, d => not(d), saida => si60);
+	u49: PortaAnd port map(a => not(a), b => b, c => c, d => d, saida => si41);
+	u50: PortaAnd port map(a => not(a), b => b, c => c, d => d, saida => si42);
+	u51: PortaAnd port map(a => not(a), b => b, c => c, d => d, saida => si43);
+	u52: PortaAnd port map(a => not(a), b => b, c => c, d => d, saida => si44);
+	u53: PortaAnd port map(a => not(a), b => b, c => c, d => d, saida => si45);
+	u54: PortaAnd port map(a => not(a), b => b, c => c, d => d, saida => si46);
+	u55: PortaAnd port map(a => not(a), b => b, c => c, d => d, saida => si47);
 	
-	u59: PortaAnd port map(a => a, b => not(b), c => c, d => not(d), saida => si43);
-	u60: PortaAnd port map(a => a, b => not(b), c => c, d => not(d), saida => si49);
-	u61: PortaAnd port map(a => a, b => not(b), c => c, d => not(d), saida => si55);
-	u62: PortaAnd port map(a => a, b => not(b), c => c, d => not(d), saida => si61);
+	u56: PortaAnd port map(a => not(a), b => b, c => not(c), d => d, saida => si48);
+	u57: PortaAnd port map(a => not(a), b => b, c => not(c), d => d, saida => si49);
+	u58: PortaAnd port map(a => not(a), b => b, c => not(c), d => d, saida => si50);
+	u59: PortaAnd port map(a => not(a), b => b, c => not(c), d => d, saida => si51);
+	u60: PortaAnd port map(a => not(a), b => b, c => not(c), d => d, saida => si52);
+	u61: PortaAnd port map(a => not(a), b => b, c => not(c), d => d, saida => si53);
+	u62: PortaAnd port map(a => not(a), b => b, c => not(c), d => d, saida => si54);
 	
-	u66: PortaAnd port map(a => a, b => b, c => not(c), d => not(d), saida => si44);
-	u67: PortaAnd port map(a => a, b => b, c => not(c), d => not(d), saida => si50);
-	u68: PortaAnd port map(a => a, b => b, c => not(c), d => not(d), saida => si56);
-	u69: PortaAnd port map(a => a, b => b, c => not(c), d => not(d), saida => si62);
+	u63: PortaAnd port map(a => not(a), b => not(b), c => c, d => d, saida => si55);
+	u64: PortaAnd port map(a => not(a), b => not(b), c => c, d => d, saida => si56);
+	u65: PortaAnd port map(a => not(a), b => not(b), c => c, d => d, saida => si57);
+	u66: PortaAnd port map(a => not(a), b => not(b), c => c, d => d, saida => si58);
+	u67: PortaAnd port map(a => not(a), b => not(b), c => c, d => d, saida => si59);
+	u68: PortaAnd port map(a => not(a), b => not(b), c => c, d => d, saida => si60);
+	u69: PortaAnd port map(a => not(a), b => not(b), c => c, d => d, saida => si61);
+	
 	
 end architecture main;
 
